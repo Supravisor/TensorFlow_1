259ae83c6fb843451035892a1dcff0ad342485d7
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-08-26 11:27:32 +1200

    fix(tensor): alert for variable.value

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let rank = document.getElementById("rank");
 
 const tensor = stat => {
   if (variable.value === "") {
-    return alert("Please enter a variable name in the 'Create Tensor' section");
+    return alert("Please enter a variable name in the 'variable' field, in the 'Create Tensor' section");
   } else if (value.value === "") {
       return alert("Please enter a value in the 'Create Tensor' section");
   }
