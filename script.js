9060aba34621254c5bc62ae3ecce4ccd6e4f39d6
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-08-25 14:17:57 +1200

    add rank variable

--- script.js
+++ script.js
@@ -3,3 +3,4 @@
 let variable = document.getElementById("variable");
 let value = document.getElementById("value");
 let datatype = document.editor.datatype;
+let rank = document.getElementById("rank");
