ccbe0162561f14e1e2538d1b6170ea67d7246786
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-08-26 11:25:25 +1200

    add an else if statement for value.value

--- script.js
+++ script.js
@@ -8,5 +8,7 @@ let rank = document.getElementById("rank");
 const tensor = stat => {
   if (variable.value === "") {
     return alert("Please enter a variable name in the 'Create Tensor' section");
+  } else if (value.value === "") {
+      return alert("Please enter a value in the 'Create Tensor' section");
   }
 }
