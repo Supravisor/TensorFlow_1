2aedc412dbbf46879fb117b8736de1eaed06a6ef
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-08-26 11:23:20 +1200

    add if statement for variable.value

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let value = document.getElementById("value");
 let datatype = document.editor.datatype;
 let rank = document.getElementById("rank");
 
-const tensor = (stat) => {
-
+const tensor = stat => {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'Create Tensor' section");
+  }
 }
