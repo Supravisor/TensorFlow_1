fd3bb0f860edd6330b894d0e66636c27c7053b7c
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-08-26 11:29:08 +1200

    fix(tensor): alert for value.value

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ const tensor = stat => {
   if (variable.value === "") {
     return alert("Please enter a variable name in the 'variable' field, in the 'Create Tensor' section");
   } else if (value.value === "") {
-      return alert("Please enter a value in the 'Create Tensor' section");
+    return alert("Please enter a value in the 'value' field, in the 'Create Tensor' section");
   }
 }
