6712f375e734a85789fda674714b1b2207fc1e66
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-09-01 12:30:02 +1200

    add else statement

--- script.js
+++ script.js
@@ -48,7 +48,10 @@ const tensor = stat => {
 
             if (datatype.value === "string") {
               string = nest.slice(0, -5).replace(/\s*,\s*/, ", ");
+            } else {
+
             }
+
 
           }
 
