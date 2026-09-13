be607104feb97169ad398b80b4d173ce4fcbbe31
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-09-14 10:49:18 +1200

    add variable.value if statement for reshape function

--- script.js
+++ script.js
@@ -115,7 +115,9 @@ const onesZerosFunc = (arg) => {
 }
 
 const reshape = (arg) => {
-  if (batch.value === "") {
+  if (variable.value === "") {
+    return alert("Please enter a variable name in the 'Create Tensor' section");
+  } else if (batch.value === "") {
       return alert("Please enter a number in the 'batch' field.");
   } else if (row.value === "") {
       return alert("Please enter a number in the 'row' field.");
