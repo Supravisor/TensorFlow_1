85addd4e6a1733f6a123af53202a47dc1a81224f
Supravisor <153783117+Supravisor@users.noreply.github.com>
2026-09-16 10:20:35 +1200

    add else if statement for row.value

--- script.js
+++ script.js
@@ -136,5 +136,7 @@ const reshape = (arg) => {
 const access = () => {
   if (variable.value === "") {
     return alert("Please enter a variable name in the 'Create Tensor' section.");
+  } else if (row.value === "") {
+      return alert("Please enter a number in the 'row' field in the 'Tensor functions' section.");
   }
 }
